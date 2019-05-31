1c86a84c026679455e1ee3d0ad0f9e5263aa801e
Samuel Rönnqvist <samuel.ronnqvist@gmail.com>
2019-05-31 13:18:35 +0300

    take train/dev/test sets as args, tag test set

--- train_crf.sh
+++ train_crf.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# Prepare train and validation set
 python gen_crf_feats.py $1 $2
 crfsuite learn --set=c1=35.0 --set=c2=0.5 -m crf.model -e2 crf_train.tsv crf_val.tsv
 
-#python gen_crf_feats.py $1 $3
+# Prepare test set
+python gen_crf_feats.py $1 $3
 crfsuite/frontend/crfsuite tag -m crf.model -t -s label_bias=1:0.85 crf_val.tsv
 
 crfsuite/frontend/crfsuite tag -m crf.model -s label_bias=1:0.85 crf_val.tsv > crf_val.pred
